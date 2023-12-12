bf5a23645f4551f47542da424a0bb7fe0c164a90
li-yuhai <965238844@qq.com>
2023-12-12 13:40:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
 dev: {
 		// Paths
 		assetsSubDirectory: 'static',
-		assetsPublicPath: './',
+		assetsPublicPath: '/',
 		proxyTable: {
       '/api': {
         target: 'http://127.0.0.1:8000',
